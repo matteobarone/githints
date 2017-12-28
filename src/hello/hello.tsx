76824524e5560f31327391ba99e746b706036b4e
matteobarone <matteobarone90@alice.it>
2017-12-28 17:20:23 +0100

    public/private in hello component

--- src/hello/hello.tsx
+++ src/hello/hello.tsx
@@ -14,38 +14,39 @@ export class Hello extends React.Component<HelloProps, HelloState> {
   constructor(props: HelloProps, private timerID: any) {
     super(props);
     this.state = {time: new Date(), passed: 0};
+    this.reset = this.reset.bind(this);
   }
 
-  componentDidMount(): void {
+  public componentDidMount(): void {
     this.timerID = setInterval(
       () => this.tick(this.state.passed),
       1000
     );
   }
 
-  componentWillUnmount(): void {
+  public componentWillUnmount(): void {
     clearInterval(this.timerID);
   }
 
-  tick(t: number): void {
+  public render() {
+    return <h1>
+      Hello from {this.props.title}!<br/>
+      {this.state.time.toLocaleTimeString()}<br/>
+      Here from {this.state.passed} seconds<br/>
+      <span onClick={this.reset}>reset</span>
+    </h1>;
+  }
+
+  private tick(t: number): void {
     this.setState({
       time: new Date(),
       passed: t + 1
     });
   }
 
-  public reset(): void {
+  private reset(): void {
     this.setState({
       passed: 0
     });
-  }
-
-  render() {
-    return <h1>
-      Hello from {this.props.title}!<br/>
-      {this.state.time.toLocaleTimeString()}<br/>
-      Here from {this.state.passed} seconds<br/>
-      <span onClick={this.reset.bind(this)}>reset</span>
-    </h1>;
   }
 }
